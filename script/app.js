5656217b236d2a3ee506cb86e1707b6285fd2bb8
yahya <ilkdoganyahya@gmail.com>
2022-07-20 01:52:19 +0300

    edit commit

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@
 const prices = document.querySelectorAll(".price")
 const bars = document.querySelectorAll(".bar")
 const days = document.querySelectorAll(".day")
+const errorMessage = document.getElementById("card-content-header")
 
 bars.forEach((bar)=>{
     bar.addEventListener("mouseenter", ()=>{
@@ -15,13 +16,18 @@ bars.forEach((bar)=>{
 
 const getData = () => {
     fetch("data.json")
-        .then((response) =>response.json())
+        .then((response) => {
+            if(!response.ok)
+                throw new Error("Information not found")
+            return response.json()
+        })
         .then((data) => {
             for(var i = 0; i < data.length; i++) {
                 prices[i].innerText = data[i].amount
                 bars[i].style.height = `${data[i].amount * 3}px`
                 days[i].innerText = data[i].day
             }
-        })
+        }).catch((err) => errorMessage.innerText = (err))
+
 }
 getData()
